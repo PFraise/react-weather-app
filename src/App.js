174ee9ba616e4f66e793315ab2ebb69efc0cf29c
PFraise <karen.gammarota@gmail.com>
2024-06-22 10:07:13 +0100

    added GH repo url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Karen Gammarota and is{" "}
+        <a href="https://github.com/PFraise/react-weather-app" target="_blank">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
