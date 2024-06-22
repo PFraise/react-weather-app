0224ea56b7e508f13b81d92d43c48a216d0062e2
PFraise <karen.gammarota@gmail.com>
2024-06-22 10:20:04 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Karen Gammarota and is{" "}
-        <a href="https://github.com/PFraise/react-weather-app" target="_blank">
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is created by Karen Gammarota and is{" "}
+          <a
+            href="https://github.com/PFraise/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
